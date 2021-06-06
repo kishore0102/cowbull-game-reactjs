e9b18a7e518344f476d4731816d042b6f42c283d
Kishore Ramakrishnan <rkishore0102@gmail.com>
2021-06-07 00:35:24 +0530

    v1 - updated refresh for guess input

--- src/components/guessinput.component.js
+++ src/components/guessinput.component.js
@@ -63,10 +63,10 @@ export default class GuessInput extends Component {
     let word = this.letter1 + this.letter2 + this.letter3 + this.letter4;
     this.props.handleAddGuessedWords(word);
     document.getElementById("guess-input-form").reset();
-    this.letter1 = "";
-    this.letter2 = "";
-    this.letter3 = "";
-    this.letter4 = "";
+    this.letter1 = null;
+    this.letter2 = null;
+    this.letter3 = null;
+    this.letter4 = null;
     this.setState({ guessButtonDisabled: true });
   };
 
